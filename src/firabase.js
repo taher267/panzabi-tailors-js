9dd6f0cbad6d538557be9e5f6d629d1e8aaea909
ABU TAHER <abutaher267@gmail.com>
2022-08-01 21:55:52 +0600

    Using email address hints for firebase

--- src/firabase.js
+++ src/firabase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+//Using email address hints pzcom
 const app = initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
